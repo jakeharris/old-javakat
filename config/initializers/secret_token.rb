893e9ff68cfb8c3096307a522c6b41161ccc2a83
javakat <javakatdesign@gmail.com>
2013-08-26 15:56:55 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Javakat::Application.config.secret_key_base = '0b6120cd8e54f449b1bdccb7ebc4629e6e6cefc017d3825923cdfb9e116a8ca780642653255acbdd0c6e0eba5d9813d0d0a92840660dffa40b6a8419bde7d079'
