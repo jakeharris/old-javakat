a8c002ad34880876b92ffa8001a786cf150ffa60
Jake <javakatdesign@gmail.com>
2014-03-09 22:28:50 -0500

    Fix app.js, make it actually listen
    lol whoops

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express')
   , app = express()
+  , port = process.env.PORT || 8080
 
 app.get('/', function(req, res) {
   res.send('./app/index.html')
+})
+
+app.listen(port, function () {
+  console.log('Listening on ' + port)
 })
