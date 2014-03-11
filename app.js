acd6cf0919c1cfbe228f437b08e0e916ec0d6e91
Jake <milieu@javakat.io>
2014-03-11 00:19:22 +0000

    Finish basic node translation

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express')
   , app = express()
   , port = process.env.PORT || 8080
 
+app.set('views', __dirname + '/app')
+app.set('view engine', 'html')
+app.engine('html', require('ejs').renderFile)
+app.use(express.static('app/assets/'))
+
 app.get('/', function(req, res) {
-  res.send('./app/index.html')
+  res.render('index')
 })
 
 app.listen(port, function () {
